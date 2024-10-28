0d33c3f548c852a0fc2c0c9346005c078efbe677
SR0204 <k023g0023@g.neec.ac.jp>
2024-10-28 16:42:31 +0900

    03-01終わり

--- Input.h
+++ Input.h
@@ -16,6 +16,7 @@ public:
 	//初期化
 	void Initialize(HINSTANCE hInstance, HWND hwnd);
 
+
 	//更新
 	void Update();
 
---
+++ WinApp.h
@@ -0,0 +1,15 @@
+#pragma once
+#include"Windows.h"
+
+class WinApp
+{
+public:
+	//初期化
+	void Initialize();
+
+	//更新
+	void Update();
+
+
+};
+
