c72309377b67a6f24ead4ae8807abcf162f852a0
SR0204 <k023g0023@g.neec.ac.jp>
2024-11-21 12:50:03 +0900

    file.openのエラーが出る

--- Particle.PS.hlsl
+++ Particle.PS.hlsl
@@ -2,14 +2,14 @@
 
 struct Pixelshaderoutput
 {
-    float4 color : SV_TARGET0;
+    float32_t4 color : SV_TARGET0;
 
 };
-Texture2D<float4> gTexture : register(t0);
+Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 struct Material
 {
-    float4 color;
+    float32_t4 color;
 };
 ConstantBuffer<Material> gMaterial : register(b0);
 
@@ -20,7 +20,7 @@ Pixelshaderoutput main(VertexShaderOutput input)
 {
     Pixelshaderoutput output;
     output.color = gMaterial.color; //float32_t4(1.0, 1.0, 1.0, 1.0);
-    float4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     output.color = gMaterial.color * textureColor;
 
 	//output.colorのa値が0の時にPixelを棄却
--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -1,8 +1,8 @@
 #include"Particle.hlsli"
 struct TransformationMatrix
 {
-    float4x4 WVP;
-    float4x4 World;
+    float32_t4x4 WVP;
+    float32_t4x4 World;
 };
 StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
 
@@ -12,16 +12,14 @@ StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
 
 struct VertexShaderInput
 {
-    float4 position : POSITION0;
-    float2 texcoord : TEXCOORD0;
+    float32_t4 position : POSITION0;
+    float32_t2 texcoord : TEXCOORD0;
 };
 
-VertexShaderOutput main(VertexShaderInput input, uint instanceId : SV_InstanceID)
+VertexShaderOutput main(VertexShaderInput input, uint32_t instanceId : SV_InstanceID)
 {
     VertexShaderOutput output;
     output.position = mul(input.position, gTransformationMatrices[instanceId].WVP); //output.position = input.position
     output.texcoord = input.texcoord;
     return output;
 }
-
-
