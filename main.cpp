5ba9f80beb1bf28ecdbe3c68fbd2e577227ff0d4
SR0204 <k023g0023@g.neec.ac.jp>
2024-12-06 10:35:26 +0900

    ウィンドウタイトルを変えた

--- main.cpp
+++ main.cpp
@@ -537,7 +537,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {//main関数
 	//ウィンドウの生成
 	HWND hwnd = CreateWindow(
 		wc.lpszClassName,
-		L"CG2",
+		L"LE2C_12_Suzuki_Reo",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
 		CW_USEDEFAULT,
