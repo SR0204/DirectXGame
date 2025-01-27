b6ef9455be56d10881d36a9dfa0cb8b25cd8d5bd
SR0204 <k023g0023@g.neec.ac.jp>
2025-01-27 15:23:09 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -989,7 +989,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {//main関数
 	std::memcpy(vertexData, modelDate.vertices.data(), sizeof(VertexData) * modelDate.vertices.size());//頂点リソースをコピー
 
 	////左下
-	//vertexData[0].position = { -0.5f,-0.5f,0.0f,1.0f };
+	//[vertexData[0].position = { -0.5f,-0.5f,0.0f,1.0f };
 	//vertexData[0].texcoord = { 0.0f,1.0f };
 	////上
 	//vertexData[1].position = { 0.0f,0.5f,0.0f,1.0f };
